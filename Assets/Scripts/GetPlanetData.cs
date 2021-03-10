b1d41694ade08a3909fcab059e09f32cb812f6ad
unknown <tappannagari@gmail.com>
2021-03-10 12:48:41 -0800

    Adding API Requests

---
+++ Assets/Scripts/GetPlanetData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetPlanetData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+    	// Buttons from panel will control the data so that the request can be made to get Planet Data
+    	// Usage of API: https://api.le-system-solaire.net/rest/bodies/<planet name>
+    	StartCoroutine(GetRequest("https://api.le-system-solaire.net/rest/bodies/Earth"));
+    }
+    
+    IEnumerator GetRequest(string uri){
+    	UnityWebRequest uwr = UnityWebRequest.Get(uri);
+    	yield return uwr.SendWebRequest();
+
+    	if(uwr.isNetworkError){
+    		Debug.Log("Error " + uwr.error);
+    	}
+    	else{
+    		Debug.Log("200 " + uwr.downloadHandler.text);
+    	}
+    }
+}
