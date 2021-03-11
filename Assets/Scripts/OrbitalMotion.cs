9b1514c7d6050dc5bd4b0a5072fc42fcac46855a
Ben Berryman <ben.a.berryman@gmail.com>
2021-03-11 17:27:33 -0600

    Cleanup + Orbital Motion
    Cleaned world positioning and object hierarchy, as well as implemented orbital motion

---
+++ Assets/Scripts/OrbitalMotion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalMotion : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed /= 500;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0, Space.World);
+    }
+}
