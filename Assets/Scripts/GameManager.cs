24ed9a5720c730b8a3c58fe990d117bfb3371039
Nicholas Scott <nikuhluss@gmail.com>
2021-03-20 23:27:36 -0500

    previous button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,15 @@ public class GameManager : MonoBehaviour
     //button1 will do an action to change what is on the screen, whatever is then displayed will determine the data that is shown to the user
     //button2
     // Start is called before the first frame update
-    public Button yourButton;
-    
+    public Button nextButton;
+    public Button prevButton;
+
     public GameObject[] planetScenes;
     void Start()
     {
-        yourButton.onClick.AddListener(next);
+        nextButton.onClick.AddListener(next);
+        prevButton.onClick.AddListener(previous);
+
         for (int i = 1; i < planetScenes.Length; i++) {
             planetScenes[i].SetActive(false);
         }
@@ -22,7 +25,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void next()
     {
-        Debug.Log("called");
         for (int i = 0; i < planetScenes.Length; i++)
         {
             Debug.Log(planetScenes[i].activeInHierarchy);
@@ -34,4 +36,27 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+
+    void previous() 
+    {
+        for (int i = planetScenes.Length-1; i >= 0; i--)
+        {
+            Debug.Log(planetScenes[i].activeInHierarchy);
+            if (planetScenes[i].activeInHierarchy)
+            {
+                planetScenes[i].SetActive(false);
+                Debug.Log("calculated: " + ((i - 1) % planetScenes.Length));
+                int newIndex = i - 1;
+                if (newIndex < 0)
+                {
+                    planetScenes[planetScenes.Length - 1].SetActive(true);
+                }
+                else {
+                    planetScenes[(i - 1) % planetScenes.Length].SetActive(true);
+                }
+                break;
+            }
+        }
+    }
+
 }
